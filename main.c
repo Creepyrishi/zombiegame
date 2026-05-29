22aad2dfcf0205c9a1342cdcdbeb5a24866bb4e5
Creepyrishi <rishi1999xyz@gmail.com>
2026-05-29 08:01:50 +0545

    Player movement

--- main.c
+++ main.c
@@ -2,6 +2,28 @@
 #include <stdatomic.h>
 #include <stdio.h>
 
+
+void reload(int *bulletCount, Sound reloadSound) {
+    *bulletCount = 10;
+    PlaySound(reloadSound);
+}
+
+void characterControl(Vector2 *playerPos) {
+    // Character Control
+    if (IsKeyDown(KEY_W)) {
+        playerPos->y -= 5; // The arrow operator is equivalent to (*playerPos).y -= 5;
+    }
+    if (IsKeyDown(KEY_S)) {  
+        playerPos->y += 5;
+    }
+    if (IsKeyDown(KEY_A)) {
+        playerPos->x -= 5;
+    }
+    if (IsKeyDown(KEY_D)) {
+        playerPos->x += 5;
+    }
+}
+
 int main(void)
 {
     InitWindow(800, 450, "Dungeon Of Jhur");
@@ -10,11 +32,29 @@ int main(void)
     printf("Audio device ready: %d\n", IsAudioDeviceReady());
     
     Texture2D customMouse = LoadTexture("assets/sprites/cursor.png");
+    Texture2D player = LoadTexture("assets/sprites/character/test.png");
+    Texture2D enemy = LoadTexture("assets/sprites/enemy/Zombie Woman/test.png");
+
+    //audio
     Sound fireSound = LoadSound("assets/sounds/gun_fire.wav");
     Sound reloadSound = LoadSound("assets/sounds/reload.wav");
-    printf("Sound frames: %d\n", fireSound.frameCount);
+    Sound bgZombie = LoadSound("assets/sounds/bg_zombie.wav");
+    Sound playerDeath = LoadSound("assets/sounds/emotional_damage.wav");
+    Sound enemyDeath = LoadSound("assets/sounds/enemy_death.wav");
+    Sound enemySpawn = LoadSound("assets/sounds/enemy_spwan.wav");
+    
+    
+    // Initialize positions
+    Vector2 playerPos = {50, 50};
+    Vector2 enemyPos = {60, 50};
+
+    float enemySpeed = 2.5;
+    
+    int playerHealth = 100;
+    int bulletCount = 0;
 
     SetTargetFPS(60);
+    reload(&bulletCount, reloadSound);
 
     while (!WindowShouldClose())
     {   
@@ -32,32 +72,64 @@ int main(void)
 
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
             printf("LEFT click detected\n Reloading sound...\n");
-            PlaySound(reloadSound);
+            reload(&bulletCount, reloadSound);
         }
         
         if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
             printf("RIGHT click detected\n Firing...\n");
-            PlaySound(fireSound);
+            if (bulletCount > 0) {
+                bulletCount -= 1;
+                PlaySound(fireSound);
+            } else {
+                reload(&bulletCount, reloadSound);
+            }
         }
-        
-        if (IsMouseButtonPressed(MOUSE_BUTTON_MIDDLE)) {
-            printf("MIDDLE click detected\n");
-            PlaySound(fireSound);
-        }
-        
-        ClearBackground(DARKGRAY);
-        DrawText("Raylib is working!", 250, 200, 30, RAYWHITE);
-        DrawText("Click any mouse button!", 200, 250, 20, RAYWHITE);
         
 
+        ClearBackground(DARKGRAY);
+
+        // Character Control
+        characterControl(&playerPos);
+
+
+        // enemy movement
+        if (enemyPos.x > playerPos.x) {
+            enemyPos.x -= enemySpeed;
+        }
+        if (enemyPos.x < playerPos.x) {
+            enemyPos.x += enemySpeed;
+        }
+        if (enemyPos.y > playerPos.y) {
+            enemyPos.y -= enemySpeed;
+        }
+        if (enemyPos.y < playerPos.y) {
+            enemyPos.y += enemySpeed;
+        }
+        
+        
+
+        // Draw player
+        DrawTexture(player, (int)playerPos.x, (int)playerPos.y, WHITE);
+        
+        // Draw enemy
+        DrawTexture(enemy, (int)enemyPos.x, (int)enemyPos.y, WHITE);
         
         // Drawinng Mouse cursor is nessary just before EndDrawing() for overlapping issues
         DrawTexture(customMouse, (int)mousePos.x, (int)mousePos.y, WHITE);
+    
         EndDrawing();
     }
 
     UnloadSound(fireSound);
+    UnloadSound(reloadSound);
+    UnloadSound(bgZombie);
+    UnloadSound(playerDeath);
+    UnloadSound(enemyDeath);
+    UnloadSound(enemySpawn);
     UnloadTexture(customMouse);
+    UnloadTexture(player);
+    UnloadTexture(enemy);
+    
     CloseAudioDevice();
     CloseWindow();
     return 0;
